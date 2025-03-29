cff3dc6f46a3bbe6a6b9a9e7aa53269c5d962073
yonghun16 <yonghun16@naver.com>
2025-03-30 07:55:57 +0900

    네비게이션 컴포넌트 스크롤 구현

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
 
 const Nav = () => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if(window.scrollY > 50) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener('scroll', () => {});
+    }
+  }, [])
+  
   return (
-    <NavWrapper>
-      <img
-        src="/images/logo.svg"
-        alt="Digenie+ Logo"
-        onClick={() => (window.location.href = "/")}
-      />
+    <NavWrapper $show={show}>
+      <Logo>
+        <img
+          src="/images/logo.svg"
+          alt="Digenie+ Logo"
+          onClick={() => (window.location.href = "/")}
+        />
+      </Logo>
     </NavWrapper>
   )
 }
@@ -20,7 +38,7 @@ const NavWrapper = styled.nav`
   left: 0;
   right: 0;
   height: 70px;
-  background-color: #090b13;
+  background-color: ${(props) => (props.$show ? "#090b13" : "transparent")};
   display: flex;
   justify-content: space-between;
   align-items: center;
