e63818ec0ff6dd08ba8cda9966f895a3f7f6ad33
yonghun16 <yonghun16@naver.com>
2025-03-31 08:49:10 +0900

    ROW 컴포넌트 CSS

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import axios from '../api/axios'
+import './Row.css'
 
 const Row = ({ title, id, fetchUrl }) => {
   const [movies, setMovies] = useState([]);
@@ -17,11 +18,15 @@ const Row = ({ title, id, fetchUrl }) => {
     <div>
       <h2>{title}</h2>
       <div className="slider">
+
+        {/* 왼쪽 화살표*/}
         <div className="slider__arrow-left">
           <span className="arrow">
             {'<'}
           </span>
         </div>
+
+        {/* 영화 */}
         <div id={id} className="row__posters">
           {movies.map(movie => (
             <img
@@ -32,11 +37,14 @@ const Row = ({ title, id, fetchUrl }) => {
             />
           ))}
         </div>
+
+        {/* 오른쪽 화살표 */}
         <div className="slider__arrow-right">
           <span className="arrow">
             {'>'}
           </span>
         </div>
+
       </div>
     </div>
   )
