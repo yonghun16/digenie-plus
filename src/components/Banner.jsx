6b01864e9793f22377d549d9535c1877326006be
yonghun16 <yonghun16@naver.com>
2025-03-30 11:57:08 +0900

    이미지 배너 생성

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from '../api/axios'
 import requests from '../api/requests'
+import './Banner.css'
 
 const Banner = () => {
   const [movie, setMovie] = useState([]);
@@ -25,8 +26,40 @@ const Banner = () => {
     setMovie(movieDetail);
   }
 
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substring(0, n - 1) + "..." : str;
+  }
+
   return (
-    <div>Banner</div>
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+        backgroundPosition: "top center",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie.title || movie.name || movie.original_name}
+        </h1>
+
+        <div className="banner__buttons">
+          {movie?.videos?.results[0]?.key &&
+            <button
+              className="banner__button"
+            >
+              Play
+            </button>
+          }
+        </div>
+
+        <p className="banner__description">
+          {truncate(movie.overview, 100)}
+        </p>
+      </div>
+      <div className="banner--fadeBottom"></div>
+    </header>
   )
 }
 
