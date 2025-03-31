84d69a9eb13ae0182cd9619542fc9ba2547a857f
yonghun16 <yonghun16@naver.com>
2025-03-31 10:32:27 +0900

    영화 자세히 보기 모달 생성하기

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import axios from '../api/axios'
 import './Row.css'
+import MovieModal from './MovieModal/MovieModal';
 
 const Row = ({ title, id, fetchUrl }) => {
   const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelection] = useState({});
 
   const fetchMovieData = useCallback(async () => {
     const response = await axios.get(fetchUrl);
@@ -13,6 +16,11 @@ const Row = ({ title, id, fetchUrl }) => {
   useEffect(() => {
     fetchMovieData();
   }, [fetchMovieData]);
+
+  const handleClick = (movie) => {
+    setModalOpen(true);
+    setMovieSelection(movie);
+  }
 
   return (
     <div>
@@ -39,6 +47,7 @@ const Row = ({ title, id, fetchUrl }) => {
               className="row__poster"
               src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
               alt={movie.name}
+              onClick={() => handleClick(movie)}
             />
           ))}
         </div>
@@ -56,6 +65,12 @@ const Row = ({ title, id, fetchUrl }) => {
         </div>
 
       </div>
+
+      {setModalOpen && 
+        <MovieModal
+          {...movieSelected}
+        />
+      }
     </div>
   )
 }
