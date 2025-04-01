a258840c03f76f7a5a0f815fe94f22aed3387c69
yonghun16 <yonghun16@naver.com>
2025-04-01 09:17:27 +0900

    deploy

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,36 @@
 import React, { useState, useEffect } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom';
 import styled from 'styled-components'
+import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut } from 'firebase/auth';
 
 const Nav = () => {
   const [show, setShow] = useState(false);
   const { pathname } = useLocation();
   const [searchValue, setSearchValue] = useState("");
   const navigate = useNavigate();
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+
+  const initialUserData = localStorage.getItem("userData")
+    ? JSON.parse(localStorage.getItem("userData")) 
+    : {};
+  const [userData, setUserData] = useState(initialUserData);
+
+
+
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        if (pathname === "/" || pathname === "/login") {
+          navigate("/main");
+        }
+      }
+      else {
+        navigate("/");
+      }
+    })
+  }, [auth, navigate, pathname])
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
@@ -16,7 +40,7 @@ const Nav = () => {
   }, [])
 
   const handleScroll = () => {
-    if(window.scrollY > 50) {
+    if (window.scrollY > 50) {
       setShow(true);
     } else {
       setShow(false);
@@ -27,7 +51,29 @@ const Nav = () => {
     setSearchValue(e.target.value);
     navigate(`/search?q=${e.target.value}`);
   }
-  
+
+  const handleAuth = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => { 
+        setUserData(result.user);
+        localStorage.setItem("userData", JSON.stringify(result.user));
+      })
+      .catch((error) => {
+        clonsole.log(error);
+      })
+  }
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUserData({});
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
   return (
     <NavWrapper $show={show}>
       <Logo>
@@ -37,21 +83,69 @@ const Nav = () => {
           onClick={() => (window.location.href = "/")}
         />
       </Logo>
-     {pathname === "/" ? 
-        (<Login>Login</Login>): 
-        <Input 
-          value={searchValue}
-          onChange={handleChange}
-          className='nav__input' 
-          type="text" 
-          placeholder='검색해주세요.' 
-        />
-      } 
+
+      {pathname === "/" ?
+        (<Login onClick={handleAuth}>Login</Login>) :
+        <>
+          <Input
+            value={searchValue}
+            onChange={handleChange}
+            className='nav__input'
+            type="text"
+            placeholder='검색해주세요.'
+          />
+
+          <SignOut>
+            <UserImg src={userData.photoURL} />
+            <DropDown>
+              <span onClick={handleSignOut}>Sign out</span>
+            </DropDown>
+          </SignOut>
+        </>
+      }
     </NavWrapper>
   )
 }
 
 export default Nav
+
+const DropDown = styled.div`
+  position: absolute;
+  top: 48px;
+  right: 0px;
+  background: rgb(19, 19, 19)
+  border: 1px solid rgba(151, 151, 151, 0.34);
+  border-radius:  4px;
+  box-shadow: rgb(0 0 0 /50%) 0px 0px 18px 0px;
+  padding: 10px;
+  font-size: 14px;
+  letter-spacing: 3px;
+  width: 100%;
+  opacity: 0;
+`;
+
+const SignOut = styled.div`
+  position: relative;
+  height: 48px;
+  width: 48px;
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    ${DropDown} {
+      opacity: 1;
+      transition-duration: 1s;
+    }
+  }
+`;
+
+const UserImg = styled.img`
+  border-radius: 50%;
+  width: 100%;
+  height: 100%;
+`;
 
 const Login = styled.a`
   background-color: rgba(0, 0, 0, 0.6);
